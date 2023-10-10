bc72ee2eed6b1751220e8c8bc3f74afd4b58ebe1
vitorbizarra <vitorbizarracontato@gmail.com>
2023-10-09 23:42:46 -0300

    :sparkles: feat: Users Endpoints

--- src/main/java/one/digitalinnovation/gof/models/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package one.digitalinnovation.gof.models;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskRepository extends CrudRepository<Task, Long> {
-
-}
--- src/main/java/one/digitalinnovation/gof/models/User.java
+++ src/main/java/one/digitalinnovation/gof/models/User.java
@@ -1,14 +1,9 @@
 package one.digitalinnovation.gof.models;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 
 @Entity
 public class User {
@@ -17,9 +12,6 @@ public class User {
     private Long id;
 
     private String name;
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-    private List<Task> tasks = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -31,13 +23,5 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public List<Task> getTasks() {
-        return tasks;
-    }
-
-    public void setTasks(List<Task> tasks) {
-        this.tasks = tasks;
     }
 }
